eddea3b9a00630702152cc5f060174daba5da278
Cass May <ingeniousmammal@gmail.com>
2016-12-25 00:43:59 +0000

    Miraculously, still discovering correct spelling of 'palette'

--- scripts/genPalette.py
+++ scripts/genPalette.py
@@ -8,37 +8,39 @@ from rgb2hsi import rgb2hsi, HSI
 
 if len(sys.argv) != 4:
     print("""ERROR: incorrect usage.
-    correct usage: genPallette.py palletteFile.bmp palletteName palletteLength""")
+    correct usage: genPalette.py paletteFile.bmp paletteName paletteLength""")
     exit(1)
 
 bmpFile = sys.argv[1]
-palletteName = sys.argv[2]
-palletteLength = int(sys.argv[3])
+paletteName = sys.argv[2]
+paletteLength = int(sys.argv[3])
 
 if not os.path.isfile(bmpFile):
     print("ERROR: the file {} does not exist".format(bmpFile))
 
 image = imread(bmpFile, flatten=0, mode="RGB")
 imageLength = image.shape[1]
-pixelsPerOutput = float(imageLength) / float(palletteLength)
+pixelsPerOutput = float(imageLength) / float(paletteLength)
 
 output = []
 hsiOutput = []
 
-for i in range(palletteLength):
+for i in range(paletteLength):
     output.append([])
 
     kernelSize = pixelsPerOutput
 
+    # hsiOutput.append(rgb2hsi(image[0][int(i*pixelsPerOutput)]))
     # take cubic resample of input
     for j in range(3):
         """
         output[i].append(image[0][int(i*pixelsPerOutput)][j]*4)
 
         """
+
         result = 0
 
-        for k in range(int(-kernelSize), int(kernelSize)):
+        for k in range(int(-kernelSize), int(kernelSize)+1):
             x = abs(k / (kernelSize/2.0)) # gives us an r value between 0 and 2
             b = 1/3.0
             c = 1/3.0 # use a mitchell filter to start with
@@ -58,31 +60,32 @@ for i in range(palletteLength):
             elif index < 0:
                 index += imageLength
 
-            result += weighting * image[0][index][j]
+            result += weighting * float(image[0][index][j])
 
         output[i].append(result)
 
 
 scalingFactor = 1023./float(max(map(max,output)))
 
+for i in hsiOutput:
+    i.i = 1 - ((1 - i.i) * (1-minAcceptableIntensity)/(1-minIntensity))
+
 for i in output:
     hsiOutput.append(rgb2hsi(i))
 
-for pixel in range(palletteLength):
+for pixel in range(paletteLength):
     for colour in range(3):
         output[pixel][colour] *= scalingFactor
         # output[pixel][colour] = gammaVals[int(output[pixel][colour])]
 
-
-
 # create preview image before adding colour correction
-rgbOut = numpy.zeros((40, palletteLength, 3), dtype=numpy.uint8)
+rgbOut = numpy.zeros((40, paletteLength, 3), dtype=numpy.uint8)
 for i in range(40):
-    for j in range(palletteLength):
+    for j in range(paletteLength):
         for k in range(3):
             rgbOut[i][j][k] = int(output[j][k]/4)
 
-imsave(palletteName+'_gradient.png', rgbOut)
+imsave(paletteName+'_gradient.png', rgbOut)
 
 # colour correction
 correctionFactors = [1., 0.69, 0.94, 0.8]
@@ -93,17 +96,17 @@ warmWhiteComposition = [1023., 856., 680.]
 
 
 
-for i in range(palletteLength):
+for i in range(paletteLength):
     levels = []
+    # for j in range(3):
+        # levels.append(output[i][j] / warmWhiteComposition[j])
+
+    minLevel = min(output[i])
+
     for j in range(3):
-        levels.append(output[i][j] / warmWhiteComposition[j])
+        output[i][j] = int(output[i][j] - (minLevel))
 
-    minLevel = min(levels)
-
-    for j in range(3):
-        output[i][j] = int(output[i][j] - (minLevel*warmWhiteComposition[j]))
-
-    output[i].append(int(minLevel*1024))
+    output[i].append(int(minLevel))
 
     """
     minLevel = min(output[i])
@@ -118,13 +121,13 @@ for i in range(palletteLength):
 
 
 
-f = open(palletteName+"_pallette.h", 'w')
+f = open(paletteName+"_palette.h", 'w')
 
-f.write("#ifndef {0}\n#define {0}\n\n// RGBW file generated via genPallette.py, created by Cass May\n\n#include <stdint.h>\n\n".format(palletteName.upper()+"_PALLETTE_H"))
+f.write("#ifndef {0}\n#define {0}\n\n// RGBW file generated via genPalette.py, created by Cass May\n\n#include <stdint.h>\n\n".format(paletteName.upper()+"_PALETTE_H"))
 
-f.write("int {}PalletteLen = {};\n".format(palletteName, palletteLength))
-f.write("uint16_t {}[{}][4] = {};\n\n".format(palletteName+"RgbwPallette", palletteLength, str(output).replace('[', '{').replace(']','}')))
-f.write("double {}[{}][3] = {};\n\n".format(palletteName+"HsiPallette", palletteLength, str(hsiOutput).replace('[', '{').replace(']','}')))
+f.write("int {}PaletteLen = {};\n".format(paletteName, paletteLength))
+f.write("uint16_t {}[{}][4] = {};\n\n".format(paletteName+"RgbwPalette", paletteLength, str(output).replace('[', '{').replace(']','}')))
+f.write("double {}[{}][3] = {};\n\n".format(paletteName+"HsiPalette", paletteLength, str(hsiOutput).replace('[', '{').replace(']','}')))
 
 f.write("#endif\n")
 
