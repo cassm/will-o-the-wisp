4771ac254f42cb3cdbeec7d30776851b2356a34e
Cass May <ingeniousmammal@gmail.com>
2017-03-26 14:25:33 +0100

    Add basic control script based on raver plaid

---
+++ fadecandyControl/lantern_control.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+from __future__ import division
+import time
+import math
+import sys
+import os
+
+cwd = os.getcwd()
+sys.path.insert(0, cwd+"/openpixelcontrol/python")
+
+import opc
+import color_utils
+import rgbw_utils
+
+
+#-------------------------------------------------------------------------------
+# handle command line
+
+if len(sys.argv) == 1:
+    IP_PORT = '127.0.0.1:7890'
+elif len(sys.argv) == 2 and ':' in sys.argv[1] and not sys.argv[1].startswith('-'):
+    IP_PORT = sys.argv[1]
+else:
+    print('''
+Usage: lantern_control.py [ip:port]
+
+If not set, ip:port defauls to 127.0.0.1:7890
+''')
+    sys.exit(0)
+
+
+#-------------------------------------------------------------------------------
+# connect to server
+
+client = opc.Client(IP_PORT)
+if client.can_connect():
+    print('    connected to %s' % IP_PORT)
+else:
+    # can't connect, but keep running in case the server appears later
+    print('    WARNING: could not connect to %s' % IP_PORT)
+print('')
+
+
+#-------------------------------------------------------------------------------
+# send pixels
+
+print('    sending pixels forever (control-c to exit)...')
+print('')
+
+n_pixels = 120  # number of pixels in the included "wall" layout
+buffer_size = rgbw_utils.get_buffer_size(n_pixels)
+fps = 60         # frames per second
+
+# print "n pixels = {} buffer size = {}".format(n_pixels, buffer_size)
+# how many sine wave cycles are squeezed into our n_pixels
+# 24 happens to create nice diagonal stripes on the wall layout
+freq_r = 24
+freq_g = 24
+freq_b = 24
+freq_w = 24
+
+# how many seconds the color sine waves take to shift through a complete cycle
+speed_r = 7
+speed_g = -13
+speed_b = 19
+speed_w = 23
+
+pixels = [[0.0, 0.0, 0.0] for i in range(buffer_size)]
+
+start_time = time.time()
+while True:
+    t = (time.time() - start_time) * 5
+    for ii in range(n_pixels):
+        pct = (ii / n_pixels)
+        # diagonal black stripes
+        pct_jittered = (pct * 77) % 37
+        blackstripes = color_utils.cos(pct_jittered, offset=t*0.05, period=1, minn=-1.5, maxx=1.5)
+        blackstripes_offset = color_utils.cos(t, offset=0.9, period=60, minn=-0.5, maxx=3)
+        blackstripes = color_utils.clamp(blackstripes + blackstripes_offset, 0, 1)
+        # 3 sine waves for r, g, b which are out of sync with each other
+        r = blackstripes * color_utils.remap(math.cos((t/speed_r + pct*freq_r)*math.pi*2), -1, 1, 0, 256)
+        g = blackstripes * color_utils.remap(math.cos((t/speed_g + pct*freq_g)*math.pi*2), -1, 1, 0, 256)
+        b = blackstripes * color_utils.remap(math.cos((t/speed_b + pct*freq_b)*math.pi*2), -1, 1, 0, 256)
+        w = blackstripes * color_utils.remap(math.cos((t/speed_w + pct*freq_w)*math.pi*2), -1, 1, 0, 256)
+        newPixel = (g, r, b, w)
+        rgbw_utils.set_pixel(pixels, ii, newPixel)
+    client.put_pixels(pixels, channel=0)
+    time.sleep(1 / fps)
+
