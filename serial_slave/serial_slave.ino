fe30a6a081298a3e9cae6101a97adf4d6e2277c5
Cass May <ingeniousmammal@gmail.com>
2017-05-13 15:30:05 +0100

    Reduce pot latency

--- serial_slave/serial_slave.ino
+++ serial_slave/serial_slave.ino
@@ -30,10 +30,17 @@ void loop() {
       Serial.println(a7_val);
   }
 
+  a2_val = analogRead(2)/8;
+  a3_val = analogRead(3)/8;
+  a4_val = analogRead(4)/8;
+  a5_val = analogRead(5)/8;
+  a6_val = analogRead(6)/8;
+  a7_val = analogRead(7)/8;
+/*
   a2_val = a2_val/8 * 7 + analogRead(2)/8;
   a3_val = a3_val/8 * 7 + analogRead(3)/8;
   a4_val = a4_val/8 * 7 + analogRead(4)/8;
   a5_val = a5_val/8 * 7 + analogRead(5)/8;
   a6_val = a6_val/8 * 7 + analogRead(6)/8;
-  a7_val = a7_val/8 * 7 + analogRead(7)/8;
+  a7_val = a7_val/8 * 7 + analogRead(7)/8;*/
 }
