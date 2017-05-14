bc4f6654966c8d2bcf2b7459794dbcaa42e4f66f
Cass May <ingeniousmammal@gmail.com>
2017-05-14 20:23:47 +0100

    Fix colour inversion transformation

--- control/rgbw_utils.py
+++ control/rgbw_utils.py
@@ -6,6 +6,13 @@ def get_buffer_size(n_pixels):
 
 
 def set_pixel(pixel_buffer, pixel_index, rgbw_val, simulate, min_level, invert):
+    if invert:
+        rgb_val = list(rgbw_val[channel] + rgbw_val[3] for channel in range(3))
+        rgb_val = list(max(255-channel, 0) for channel in rgb_val)
+        w_val = min(rgb_val)
+        rgbw_val = list(channel + w_val for channel in rgb_val)
+        rgbw_val.append(w_val)
+
     if simulate:
         g, r, b, w = rgbw_val
         pixel_buffer[pixel_index] = (r + w, g + w, b + w)
@@ -13,11 +20,7 @@ def set_pixel(pixel_buffer, pixel_index, rgbw_val, simulate, min_level, invert):
     else:
         for index, value in enumerate(rgbw_val):
             channel_index = pixel_index * 4 + index
-            if invert:
-                channel_value = 255 - min(255, max(value, min_level))
-            else:
-                channel_value = max(value, min_level)
-
+            channel_value = max(value, min_level)
             pixel_buffer[int(math.floor(channel_index / 3))][int(channel_index % 3)] = channel_value
 
 
